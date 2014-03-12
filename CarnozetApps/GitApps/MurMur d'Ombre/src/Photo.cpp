0ea997b3ec911f396bdd3eaae114c829eb00fea0
Conil Aurélien <conil.a@hotmail.fr>
2014-03-12 23:31:37 +0100

    photo

--- CarnozetApps/GitApps/MurMur d'Ombre/src/Photo.cpp
+++ CarnozetApps/GitApps/MurMur d'Ombre/src/Photo.cpp
@@ -66,6 +66,15 @@ void Photo::setupData(){
     img.loadImage("images/temps/11.jpg");
     listOfImage.push_back(img);
     
+    img.loadImage("images/temps/12.jpg");
+    listOfImage.push_back(img);
+    
+    img.loadImage("images/temps/13.jpg");
+    listOfImage.push_back(img);
+    
+    img.loadImage("images/temps/14.jpg");
+    listOfImage.push_back(img);
+    
     imgToDraw = listOfImage.front();
 }
 
@@ -75,33 +84,33 @@ void Photo::update(int w,int h){
     {
     
         
-        if(isResize)
-        {
-            
-            imgToDraw.resize(w, h);
-            
-            
-        }
-        else
-        {
-            
-            int finalHeight = h;
-            float ratioPhoto = imgToDraw.height/imgToDraw.width;
-            imgToDraw.resize(finalHeight, finalHeight*1.0f/ratioPhoto);
-            
-            if(imgToDraw.width> w)
-            {
-                float lengthToCrop = (finalHeight*1.0f/ratioPhoto) - w ;
-                imgToDraw.crop( lengthToCrop/2.0f, 0, w, h );
-                
-            }
-        
-        
-        }
-        
-        if(isMirror){
-            imgToDraw.mirror(false, true);
-        }
+//        if(isResize)
+//        {
+//            
+//            imgToDraw.resize(w, h);
+//            
+//            
+//        }
+//        else
+//        {
+//            
+//            int finalHeight = h;
+//            float ratioPhoto = imgToDraw.height/imgToDraw.width;
+//            imgToDraw.resize(finalHeight, finalHeight*1.0f/ratioPhoto);
+//            
+//            if(imgToDraw.width> w)
+//            {
+//                float lengthToCrop = (finalHeight*1.0f/ratioPhoto) - w ;
+//                imgToDraw.crop( lengthToCrop/2.0f, 0, w, h );
+//                
+//            }
+//        
+//        
+//        }
+//        
+//        if(isMirror){
+//            imgToDraw.mirror(false, true);
+//        }
        // imgToDraw.setAnchorPercent(0.5f, 0.5f);
         isSet = true;
         
@@ -115,7 +124,8 @@ void Photo::draw(int w, int h){
     if(isSet && numPhoto>0){
         
         ofSetColor(color->x, color->y, color->z);
-        imgToDraw.draw(0, 0, w, h);
+        if(isMirror) imgToDraw.draw(w,0,-w,h);
+        else imgToDraw.draw(0, 0, w, h);
     }
     
     
@@ -125,7 +135,7 @@ void Photo::changeImage(int& num){
     
     isSet = false;
     if( num< listOfImage.size()-1 && num>0){
-    imgToDraw = listOfImage.at(num+1);
+    imgToDraw = listOfImage.at(num);
     }
 
     

34c463e53c493dac35cb3639130fa1d1e42c9bc9
martin <martin.hermant@gmail.com>
2014-09-23 16:52:30 +0200

    NoSeKe

--- MTG/ViZa/src/ofApp.h
+++ MTG/ViZa/src/ofApp.h
@@ -1,28 +1,22 @@
-#ifndef _TEST_APP
-#define _TEST_APP
-
+#pragma once
 
 #include "ofMain.h"
-#include "ofxSQLiteHeaders.h"
-class testApp : public ofBaseApp{
+
+class ofApp : public ofBaseApp{
 
 	public:
-
-		testApp();
 		void setup();
 		void update();
 		void draw();
 
-		void keyPressed  (int key);
+		void keyPressed(int key);
 		void keyReleased(int key);
 		void mouseMoved(int x, int y );
 		void mouseDragged(int x, int y, int button);
 		void mousePressed(int x, int y, int button);
 		void mouseReleased(int x, int y, int button);
-		void resized(int w, int h);
-	private:
-		ofxSQLite* sqlite;
-
+		void windowResized(int w, int h);
+		void dragEvent(ofDragInfo dragInfo);
+		void gotMessage(ofMessage msg);
+		
 };
-
-#endif

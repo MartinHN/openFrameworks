e663981086a8b4bdb5b563e19bfd37e6ad5901c2
martin <martin.hermant@gmail.com>
2014-11-17 10:04:57 +0100

    addonsDependencyFixed

---
+++ addons/ofxPDF/src/ofxPDF.h
@@ -0,0 +1,35 @@
+#pragma once
+
+#include "ofMain.h"
+
+class ofxPDF
+{
+public:
+	
+	virtual ~ofxPDF();
+	
+	void load(string path, int pagenumber = 1) { loadPDF(path, pagenumber); }
+	void loadPDF(string path, int pagenumber = 1);
+	
+	void close();
+	
+	inline float getWidth() { return width; }
+	inline float getHeight() { return height; }
+	
+	size_t getNumPath() { return paths.size(); }
+	
+	ofPath & getPathAt(int n){
+		return *paths[n];
+	}
+	
+	void draw();
+    void resize(float x,float y);
+	
+private:
+	
+	float width, height;
+	
+	typedef ofPtr<ofPath> ofPathRef;
+	vector <ofPathRef> paths;
+
+};
